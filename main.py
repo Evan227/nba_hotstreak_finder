daa5e8f987a1c7d3a2ba0d42eed2ccbfda9d0fca
Evan227 <73192772+Evan227@users.noreply.github.com>
2024-03-08 13:20:34 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pandas as pd
 
-from model.hot_streak_finder import (
-    find_hotstreak_players
+from model.streak_finder import (
+    find_hot_or_cold_streak_players, create_player_average_df
 )
 
 from model.defense import team_defense_rank, matchups
 
 
 def main():
-    find_hotstreak_players()
+    find_hot_or_cold_streak_players()
+
+    # who are they playing against?
+    # defensive rating?
+    # is it a veteran vs a rookie?
 
     # https://github.com/swar/nba_api/blob/master/docs/nba_api/stats/endpoints/playervsplayer.md
     # team he plays above average against?
